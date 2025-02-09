248c1fc1d850aa917213494cc6531f9974d76f6a
Mohamed Nasr <medonasr326@gmail.com>
2025-02-09 21:59:55 +0200

    Start My App

---
+++ android/app/src/main/kotlin/Mstore/example/medostore/MainActivity.kt
@@ -0,0 +1,5 @@
+package Mstore.example.medostore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+        ),
+      ),
+    );
+  }
+}
